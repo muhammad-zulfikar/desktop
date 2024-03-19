c3873c5b2829f0941bede2991b5713bf4c8cd1c3
Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-03-20 02:30:26 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,8 +6,6 @@ module.exports = {
     domains: ['res.cloudinary.com'],
     formats: ['image/avif', 'image/webp'],
     imageSizes: [128, 256, 512, 1024, 2048],
-    unoptimized: true,
   },
   output: 'standalone',
-  basePath: '/desktop',
 };
