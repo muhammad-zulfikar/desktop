4023e5105fb6dbea2740bfd1185db99650609976
Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-03-20 02:36:02 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 // next.config.js
 
 module.exports = {
-  experimental: {images: {unoptimized: false}},basePath: "/desktop",reactStrictMode: true,
+  reactStrictMode: true,
   images: {
-    unoptimized: false,domains: ['res.cloudinary.com'],
+    domains: ['res.cloudinary.com'],
     formats: ['image/avif', 'image/webp'],
     imageSizes: [128, 256, 512, 1024, 2048],
+    unoptimized: false,
   },
   output: 'standalone',
+  basePath: '/desktop',
+};
