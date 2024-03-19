adee4a3cfa306f2a8adeffc9ab73aed64ce0592f
moonlightseonata <moonlightseonata@gmail.com>
2024-03-19 18:38:18 +0000

    updates

--- next.config.js
+++ next.config.js
@@ -6,3 +6,10 @@ module.exports = {
 		imageSizes: [128, 256, 512, 1024, 2048],
 	},
 };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	output: 'export',
+};
+
+module.exports = nextConfig;
