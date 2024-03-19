9abae96f46291b3b8e7f55d604b2232dd37e8be8
Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-03-20 03:13:21 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,9 +8,7 @@ module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
   images: {
-    domains: ['res.cloudinary.com'],
-    formats: ['image/avif', 'image/webp'],
-    imageSizes: [128, 256, 512, 1024, 2048],
+    domains: ...,
     path: `${basePath}/_next/image`,
   },
 };
