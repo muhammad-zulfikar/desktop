44c62dec1d42d58dc984a2d8c0beaaac34783200
Zulfikar <155657912+muhammad-zulfikar@users.noreply.github.com>
2024-03-20 03:01:55 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
 // next.config.js
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}/`
+}
+
 module.exports = {
-  reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     domains: ['res.cloudinary.com'],
     formats: ['image/avif', 'image/webp'],
     imageSizes: [128, 256, 512, 1024, 2048],
-    unoptimized: false,
   },
-  output: 'standalone',
-  basePath: '/desktop',
 };
